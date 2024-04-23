6e8a430603386296b4760b905b74604fb251bf84
Van Grabner <grabner737@gmail.com>
2024-04-23 17:52:29 -0500

    add boiler plate

---
+++ ekin-fanclub-galaga/src/main/java/com/oop_final_project/ekin_fanclub_galaga/App.java
@@ -0,0 +1,13 @@
+package com.oop_final_project.ekin_fanclub_galaga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
