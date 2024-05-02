19e031bd176a81c0996bb2873f962deb7ac3ff18
amelll1 <amelisa800@gmail.com>
2024-05-02 15:04:37 -0500

    added health indicators in the health package, included objects in GamePanel

--- ekin-fanclub-galaga/src/main/java/object/OBJ_Key.java
+++ ekin-fanclub-galaga/src/main/java/object/OBJ_Key.java
@@ -8,7 +8,7 @@ public class OBJ_Key extends SuperObject {
 	public OBJ_Key() {
 		name = "Key";
 		try {
-			image = ImageIO.read(getClass().getResourceAsStream("player/heart.png"));
+			image = ImageIO.read(getClass().getResourceAsStream("player/heart.png"));//path to the file
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
