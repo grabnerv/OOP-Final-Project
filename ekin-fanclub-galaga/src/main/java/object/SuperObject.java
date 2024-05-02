b0c1a79ca22c662a085174b65857e8beaa94af4c
amelll1 <amelisa800@gmail.com>
2024-05-02 14:10:03 -0500

    Created the object package and the SuperObject class

---
+++ ekin-fanclub-galaga/src/main/java/object/SuperObject.java
@@ -0,0 +1,12 @@
+package object;
+import java.awt.image.BufferedImage;
+
+
+public class SuperObject {
+	public BufferedImage image, image2, image3;
+	public String name;
+	public boolean collision = false;
+	public int worldX, worldY;
+
+
+}
